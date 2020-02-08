d798980e2c798f903561404f086ac61f6283b5c8
Enrico Costanzi <enrico.costanzi@gmail.com>
2020-02-08 12:03:17 +0100

    IASB#1 add spring boot

--- src/main/java/it/intesys/academy/SimpleApp.java
+++ src/main/java/it/intesys/academy/SimpleApp.java
@@ -1,33 +1,13 @@
 package it.intesys.academy;
 
-import it.intesys.academy.patient.Patient;
-import it.intesys.academy.patient.PatientService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.List;
-
+@SpringBootApplication
 public class SimpleApp {
 
-    private static Logger logger = LoggerFactory.getLogger(SimpleApp.class);
-
     public static void main(String[] args) {
-
-        // app entry point, will become a REST controller
-        String searchString = args[0];
-        logger.info("Searching for {}", searchString);
-
-        ApplicationContext springContext = new AnnotationConfigApplicationContext(AppConfig.class);
-
-        List<Patient> patients = springContext.getBean(PatientService.class).searchPatient(searchString);
-
-        logger.info("\nFound {} patients!", patients.size());
-
-        for (Patient patient : patients) {
-            logger.info("\t{}", patient);
-        }
-
+        SpringApplication.run(SimpleApp.class, args);
     }
+
 }
