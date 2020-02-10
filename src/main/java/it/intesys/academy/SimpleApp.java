a6fd0446c90c6de3d7fb863130105a9f5deb975f
Nicola Baiocco <nicola.baiocco@intesys.it>
2020-02-10 15:06:45 +0100

    IASB#2 manage ServerProperties with @EnableConfigurationProperties

--- src/main/java/it/intesys/academy/SimpleApp.java
+++ src/main/java/it/intesys/academy/SimpleApp.java
@@ -1,12 +1,16 @@
 package it.intesys.academy;
 
+import it.intesys.academy.config.ServerProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({ ServerProperties.class })
 public class SimpleApp {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SimpleApp.class, args);
     }
 
--- src/main/java/it/intesys/academy/config/ServerProperties.java
+++ src/main/java/it/intesys/academy/config/ServerProperties.java
@@ -3,7 +3,6 @@ package it.intesys.academy.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
 @ConfigurationProperties(prefix = "server")
 public class ServerProperties {
 
