fbdbc26ca9c030295bc65446a6a5349cb224f58e
Nicola Baiocco <nicola.baiocco@intesys.it>
2020-02-10 15:05:31 +0100

    IASB#2 add ServerProperties to manage port configuration

---
+++ src/main/java/it/intesys/academy/config/ServerProperties.java
@@ -0,0 +1,21 @@
+package it.intesys.academy.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "server")
+public class ServerProperties {
+
+    private long port;
+
+    public long getPort() {
+
+        return port;
+    }
+
+    public void setPort(long port) {
+
+        this.port = port;
+    }
+}
