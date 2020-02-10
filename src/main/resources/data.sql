53328fc7f5e6cdffdb75255ced706338e0ef3e21
Nicola Baiocco <nicola.baiocco@intesys.it>
2020-02-10 16:13:05 +0100

    IASB#2 split properties by profile and add h2 in mem for dev with fake data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO patient (id, firstName, lastName) VALUES (1, 'Enrico', 'Costanzi');
+INSERT INTO patient (id, firstName, lastName) VALUES (2, 'Tommaso', 'Moroni');
+INSERT INTO patient (id, firstName, lastName) VALUES (3, 'Nicola', 'Baiocco');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+create table patient
+(
+    id int auto_increment
+        primary key,
+    firstName varchar(255) null,
+    lastName varchar(255) null
+);
