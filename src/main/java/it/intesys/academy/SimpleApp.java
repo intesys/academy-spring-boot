062afebed609348682ea630a6d732dfb0331375f
Nicola Baiocco <nicola.baiocco@intesys.it>
2020-02-11 14:17:19 +0100

    IASB#2 add profile fake-data to manage datasource configuration

--- src/main/java/it/intesys/academy/SimpleApp.java
+++ src/main/java/it/intesys/academy/SimpleApp.java
@@ -2,7 +2,7 @@ package it.intesys.academy;
 
 import it.intesys.academy.config.ApplicationProperties;
 import it.intesys.academy.config.ServerProperties;
-import it.intesys.academy.util.DefaultProfileUtils;
+import it.intesys.academy.util.ProfileUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -14,7 +14,7 @@ public class SimpleApp {
     public static void main(String[] args) {
 
         SpringApplication app = new SpringApplication(SimpleApp.class);
-        DefaultProfileUtils.addDefaultProfile(app);
+        ProfileUtils.addDefaultProfile(app);
         app.run(args);
     }
 
--- src/main/java/it/intesys/academy/util/ProfileUtils.java
+++ src/main/java/it/intesys/academy/util/ProfileUtils.java
@@ -5,18 +5,21 @@ import org.springframework.boot.SpringApplication;
 import java.util.HashMap;
 import java.util.Map;
 
-public final class DefaultProfileUtils {
+public final class ProfileUtils {
 
     private static final String SPRING_PROFILE_DEFAULT = "spring.profiles.default";
+    public static final String DEV_PROFILE = "dev";
+    public static final String PROD_PROFILE = "prod";
+    public static final String FAKE_DATA_PROFILE = "fake-data";
 
-    private DefaultProfileUtils() {
+    private ProfileUtils() {
 
     }
 
     public static void addDefaultProfile(SpringApplication app) {
 
         Map<String, Object> defProperties = new HashMap<>();
-        defProperties.put(SPRING_PROFILE_DEFAULT, "dev");
+        defProperties.put(SPRING_PROFILE_DEFAULT, DEV_PROFILE);
         app.setDefaultProperties(defProperties);
     }
 }
