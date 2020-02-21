bc73223b512f886b0441f261f6baef43c09655ec
Enrico Costanzi <enrico.costanzi@gmail.com>
2020-02-21 18:02:37 +0100

    IASB5#E02 add flyway and create base migrations

--- src/main/resources/db/migration/V0002__create_table_examination.sql
+++ src/main/resources/db/migration/V0002__create_table_examination.sql
@@ -1,13 +1,3 @@
-create table if not exists patient
-(
-    id int auto_increment
-        primary key,
-    firstName varchar(255) null,
-    lastName varchar(255) null,
-    birthDate date not null,
-    fiscalCode char(16) not null
-);
-
 create table if not exists examination
 (
     id int auto_increment primary key,
