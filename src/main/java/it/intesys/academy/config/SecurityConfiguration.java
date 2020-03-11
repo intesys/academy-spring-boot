af6fdee0318dca77f8d73de8731407348a0fe0ab
Tommaso Moroni <tommaso.moroni@intesys.it>
2020-03-11 15:03:25 +0100

    IASB#6: Fix h2 console visibility with Spring Security

--- src/main/java/it/intesys/academy/config/SecurityConfiguration.java
+++ src/main/java/it/intesys/academy/config/SecurityConfiguration.java
@@ -1,6 +1,5 @@
 package it.intesys.academy.config;
 
-import com.fasterxml.jackson.databind.deser.impl.FieldProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,8 +7,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-import java.util.Formatter;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
