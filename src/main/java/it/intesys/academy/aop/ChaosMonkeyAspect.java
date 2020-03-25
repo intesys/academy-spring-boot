c06b1a3fa0033d4afbce344a53018f9b6780ea8a
Tommaso Moroni <tommaso.moroni@intesys.it>
2020-03-25 12:19:31 +0100

    IASB#7E03: Add ChaosMonkeyAspect

---
+++ src/main/java/it/intesys/academy/aop/ChaosMonkeyAspect.java
@@ -0,0 +1,39 @@
+package it.intesys.academy.aop;
+
+import java.util.Random;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class ChaosMonkeyAspect {
+
+    private final Random random;
+
+    public ChaosMonkeyAspect() {
+
+        random = new Random();
+    }
+
+    @Around("execution(int it.intesys.academy..*(..))")
+    public int messUp(ProceedingJoinPoint pjp)
+        throws Throwable {
+
+        int returnValue = (int) pjp.proceed();
+        if (random.nextDouble() >= 0.8) {
+            returnValue = random.nextInt();
+        }
+        return returnValue;
+    }
+
+    @AfterReturning("execution(* org.springframework.data.jpa.repository.JpaRepository+.*(..))")
+    public void throwRandomException() {
+        if (random.nextDouble() >= 0.6) {
+            throw new RuntimeException("The monkey was here!");
+        }
+    }
+}
