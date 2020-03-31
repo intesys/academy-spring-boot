aa4fe2c4f62a2d9bc9f8ad806724c06bd9399589
Enrico Costanzi <enrico.costanzi@gmail.com>
2020-03-31 17:12:51 +0200

    IASB#8E03: PatientService integration test

--- src/test/java/it/intesys/academy/patient/PatientServiceIntTest.java
+++ src/test/java/it/intesys/academy/patient/PatientServiceIntTest.java
@@ -2,17 +2,39 @@ package it.intesys.academy.patient;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
+@SpringBootTest
 public class PatientServiceIntTest {
+
+    @Autowired
+    PatientService patientService;
+
+    @Autowired
+    PatientRepository patientRepository;
 
     @Test
     @DisplayName("Case sensitive search works with patient in the database")
     void testPatientSearch() {
-        fail("not implemented");
+        //given
+        patientRepository.saveAll(List.of(
+                newPatient("Mario", "Rossi"),
+                newPatient("Maria", "Bianchi"),
+                newPatient("giuseppe", "verdi")
+        ));
+
+        //test
+        List<Patient> patients = patientService.searchPatient("Mari");
+
+        //assert
+        assertThat(patients).hasSize(2);
+        assertThat(patients).extracting("lastName").containsOnly("Rossi", "Bianchi");
     }
 
     private Patient newPatient(String firstName, String lastName) {
