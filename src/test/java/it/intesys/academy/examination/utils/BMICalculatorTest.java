691522baad19f6b18acc08772755155ae9820fd4
Enrico Costanzi <enrico.costanzi@gmail.com>
2020-03-31 16:54:16 +0200

    IASB#8E01: unit test of BMICalculator

--- src/test/java/it/intesys/academy/examination/utils/BMICalculatorTest.java
+++ src/test/java/it/intesys/academy/examination/utils/BMICalculatorTest.java
@@ -1,7 +1,10 @@
 package it.intesys.academy.examination.utils;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -11,20 +14,21 @@ class BMICalculatorTest {
     @DisplayName("test bmi calculation")
     //1.95, 85 -> 22.35
     void getBMI() {
-        fail("Not Implemented");
+        double bmi = BMICalculator.calculateBMI(85d, 1.95d);
+        assertThat(bmi).isEqualTo(22.35d);
     }
 
     @Test
     @DisplayName("Throws exeption when negative BMI is passed")
     void testNegativeBMI() {
-        fail("Not Implemented");
+        Assertions.assertThrows(IllegalArgumentException.class, ()->BMICalculator.getBMICondition(-1));
     }
 
-    @Test
     @DisplayName("Calculates multiple BMI conditions")
-    //{ "12.2, UNDERWEIGHT", "20, NORMAL", "30, OBESE"} -> "a BMI of {0} is categorized as {1}"
-    void testBmiCondition() {
-        fail("Not implemented");
+    @ParameterizedTest(name = "a BMI of {0} is categorized as {1}")
+    @CsvSource({ "12.2, UNDERWEIGHT", "20, NORMAL", "30, OBESE"})
+    void testBmiCondition(double bmi, BMICalculator.BMICondition bmiCondition) {
+        assertThat(BMICalculator.getBMICondition(bmi)).isEqualTo(bmiCondition);
     }
 
 }
