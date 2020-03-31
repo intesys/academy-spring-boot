f590e3eccd05a3dcfb986cabe9447c96c77d3d67
Enrico Costanzi <enrico.costanzi@gmail.com>
2020-03-31 18:04:41 +0200

    IASB#8E02: examination service with mockito spy

--- src/test/java/it/intesys/academy/examination/ExaminationServiceTest.java
+++ src/test/java/it/intesys/academy/examination/ExaminationServiceTest.java
@@ -1,22 +1,62 @@
 package it.intesys.academy.examination;
 
+import it.intesys.academy.examination.model.Examination;
+import it.intesys.academy.util.NotificationService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 public class ExaminationServiceTest {
 
-    @Test
-    @DisplayName("send notification when patient examination BMI is critical")
-    public void testBmiNotification() {
-        fail("Not implemented");
+    @Mock
+    ExaminationDao examinationDao;
+
+    @Spy
+    NotificationService notificationService;
+
+    private ExaminationService examinationService;
+
+    @BeforeEach
+    void setUp() {
+        examinationService = new ExaminationService(examinationDao, notificationService);
     }
 
+    @DisplayName("send notification when patient examination BMI is critical")
     @Test
+    public void testBmiNotification() {
+        //given
+        Examination examination = new Examination();
+        examination.setHeight(195);
+        examination.setWeight(200);
+
+        //test
+        examinationService.save(examination);
+
+        //verify
+        verify(notificationService, times(1)).sendCriticalExaminationEvent(examination);
+    }
+
     @DisplayName("Dont send notification when patient examination BMI is ok")
+    @Test
     public void testBmiNotificationNotSent() {
-        fail("Not implemented");
+        //given
+        Examination examination = new Examination();
+        examination.setHeight(195);
+        examination.setWeight(85);
+
+        //test
+        examinationService.save(examination);
+
+        //verify
+        verify(notificationService, times(0)).sendCriticalExaminationEvent(examination);
     }
 
 }
