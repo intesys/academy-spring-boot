60374c8f0344aaeba4a71444c68c6534b7e13dfb
Enrico Costanzi <enrico.costanzi@gmail.com>
2020-03-31 17:58:04 +0200

    IASB#8E04: DashboardController integration test with MockMvc and MockBean

--- src/test/java/it/intesys/academy/dashboard/DashboardControllerIntTest.java
+++ src/test/java/it/intesys/academy/dashboard/DashboardControllerIntTest.java
@@ -1,26 +1,30 @@
 package it.intesys.academy.dashboard;
 
+import it.intesys.academy.patient.PatientController;
+import it.intesys.academy.patient.PatientService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
 
 import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
+@WebMvcTest(controllers = {DashboardController.class, PatientController.class})
 @AutoConfigureMockMvc
-@Transactional
 class DashboardControllerIntTest {
 
     @Autowired
     MockMvc mockMvc;
+
+    @MockBean
+    PatientService patientService;
 
     @Test
     @DisplayName("Dashboard home shows 0 patients")
@@ -32,12 +36,8 @@ class DashboardControllerIntTest {
 
     @Test
     @DisplayName("Dashboard home shows 3 patients counter")
-    @Sql(statements = {
-            "INSERT INTO patient (firstName, lastName, birthDate, fiscalCode) VALUES ('Enrico', 'Costanzi', '1988-01-01', 'CSTNRC88A01L781U')",
-            "INSERT INTO patient (firstName, lastName, birthDate, fiscalCode) VALUES ('Tommaso', 'Moroni', '1981-11-07', 'MRNTMS81S07L781N')",
-            "INSERT INTO patient (firstName, lastName, birthDate, fiscalCode) VALUES ('Nicola', 'Baiocco', '1985-06-03', 'BCCNCL85H03L781Q')"
-    })
     void testHomePageWithSomePatients() throws Exception {
+        when(patientService.countPatients()).thenReturn(3);
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString("3 Patients")));
